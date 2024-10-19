cec4d7b57f9da3d761bd4e9663d533ad82b936f7
Vitu <vitorcsharp@hotmail.com>
2024-10-19 17:35:16 -0300

    feat: adiciona matToolbar ao component header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar' //-> import feito a mao
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule], //-> Componentes de package que irei utilizar nesse componente header (muitas vezes o import tem que ser escrito na mao) isso sera usado no html desse component
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
