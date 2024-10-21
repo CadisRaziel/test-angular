ea9041f067d0b809879c19f005605aa85a593342
Vitu <vitorcsharp@hotmail.com>
2024-10-21 13:24:53 -0300

    feat: adiciona listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  products: any[] = [];
 
+    httpClient = inject(HttpClient);
+
+
+    ngOnInit() {
+      // this.httpClient.get<any>('http://localhost:3000/products').subscribe((products) => {
+      this.httpClient.get<any>('/api/products').subscribe((products) => { //-> Utilizando a url com o nosso proxy     
+        this.products = products
+      });
+    }    
 }
