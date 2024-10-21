7f738558fc8e12afff1ec4f5e8d5e27a595cd81a
Vitu <vitorcsharp@hotmail.com>
2024-10-21 16:12:06 -0300

    feat: adiciona card para mostrar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,24 +1,30 @@
 import { Component, inject, Inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { ProductsModel } from '../../shared/model/products.model';
- 
+import { MatCardModule } from '@angular/material/card'; //O import ou eu coloco na mao ou eu vou la no site do material angular procurar
+import { MatButtonModule } from '@angular/material/button'; //O import ou eu coloco na mao ou eu vou la no site do material angular procurar
+
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    //Aqui irei importa os components que esse component list ira usar
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
   products: ProductsModel[] = [];
 
-    //productsService = inject(ProductsService);
-    constructor(private productsService: ProductsService) {}
+  //productsService = inject(ProductsService);
+  constructor(private productsService: ProductsService) { }
 
-     ngOnInit() {
-      // this.httpClient.get<any>('http://localhost:3000/products').subscribe((products) => {
-      this.productsService.getAll().subscribe((products) => { //-> Utilizando a url com o nosso proxy     
-        this.products = products
-      });
-    }    
+  ngOnInit() {
+    // this.httpClient.get<any>('http://localhost:3000/products').subscribe((products) => {
+    this.productsService.getAll().subscribe((products) => { //-> Utilizando a url com o nosso proxy     
+      this.products = products
+    });
+  }
 }
